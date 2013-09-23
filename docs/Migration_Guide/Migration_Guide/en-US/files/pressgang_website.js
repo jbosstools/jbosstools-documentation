0c659e494e46c348b592241bf9d09dad26072380
Michelle Murray <mmurray@redhat.com>
2013-09-23 16:33:13 +1000

    TOOLSDOC-353: Install & Migration guides - edited text about upgrading

--- docs/Migration_Guide/Migration_Guide/en-US/files/pressgang_website.js
+++ docs/Migration_Guide/Migration_Guide/en-US/files/pressgang_website.js
@@ -3,7 +3,7 @@ pressgang_website_callback([
 	{"topicId":22614,"target":"Revision_History39","title":"Revision History","newSince":""},
 	{"topicId":22615,"target":"Change_the_Version_of_Eclipse_Being_Used_with_JBoss_Developer_Studio_BYOE","title":"Change the Version of Eclipse Being Used with JBoss Developer Studio BYOE","newSince":""},
 	{"topicId":22616,"target":"Change_the_Path_of_the_JVM_used_to_Run_JBoss_Developer_Studio","title":"Change the Path of the JVM used to Run JBoss Developer Studio","newSince":""},
-	{"topicId":22601,"target":"Upgrade_JBoss_Developer_Studio_BYOE1","title":"Upgrade JBoss Developer Studio BYOE","newSince":""},
 	{"topicId":22588,"target":"Use_Cases_of_JBoss_Developer_Studio1","title":"Use Cases of JBoss Developer Studio","newSince":""},
 	{"topicId":22589,"target":"About_Red_Hat_JBoss_Developer_Studio1","title":"About Red Hat JBoss Developer Studio","newSince":""},
-	{"topicId":22604,"target":"Upgrade_JBoss_Developer_Studio1","title":"Upgrade JBoss Developer Studio","newSince":""}]);
+	{"topicId":22604,"target":"Upgrade_from_JBoss_Developer_Studio_6.x_and_Earlier","title":"Upgrade from JBoss Developer Studio 6.x and Earlier","newSince":""},
+	{"topicId":22704,"target":"Upgrade_from_JBoss_Developer_Studio_7.0","title":"Upgrade from JBoss Developer Studio 7.0","newSince":""}]);
