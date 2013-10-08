f6d9e9b78d8f09ea29bd5c18b9c7955611b544b9
Michelle Murray <mmurray@redhat.com>
2013-10-08 12:57:39 +1000

    UGToCCMS: User Guide moved to CCMS so new file names and now two books (JBDS & JBT)

--- docs/User_Guide/books.sh
+++ docs/User_Guide/books.sh
@@ -5,135 +5,72 @@
 #Script for building the User Guide
 #Builds publican and maven versions
 #
-#To make books: ./books.sh make
-#To clean books: ./books.sh clean
+#To make JBDS book with publican: ./books.sh pubmake
+#To make JBT book with maven: ./books.sh mvnmake
 #
 #############################################################
 
-<<'COMMENT'
-function make {
 
+
+function pubmake {
+	#clean last version
+	echo '[INFO] Cleaning last version'	
+	rm -rf User_Guide
+
+	#get updated content spec
+	echo '[INFO] Pulling latest content spec'
+	csprocessor pull 22443 -o User_Guide.contentspec
+	rm User_Guide.contentspec.backup
+	
 	#make publican version
 	echo '[INFO] Making publican version'
-	csprocessor build 13332
-	unzip Installation_Guide.zip
-	rm Installation_Guide.zip
-	cp Revision_History.xml Installation_Guide/en-US/.	
-	#echo 'Now edit Installation_Guide/en-US/Author.xml by hand'
-
-	#make maven version
-	echo '[INFO] Making maven version'
-	csprocessor build 13882 --server
-	unzip Red_Hat_JBoss_Developer_Studio_Installation_Guide.zip
-	rm Red_Hat_JBoss_Developer_Studio_Installation_Guide.zip
-	cp pom.xml Red_Hat_JBoss_Developer_Studio_Installation_Guide/.
-	cp Revision_History.xml Red_Hat_JBoss_Developer_Studio_Installation_Guide/en-US/.
-	cp -r Common_Content Red_Hat_JBoss_Developer_Studio_Installation_Guide/en-US/.
-	rm Red_Hat_JBoss_Developer_Studio_Installation_Guide/publican.cfg
-	sed -i '/<corpauthor>/,/<\/corpauthor>/{s/<corpauthor>//p;d}' Red_Hat_JBoss_Developer_Studio_Installation_Guide/en-US/Book_Info.xml
-
-	#give feeback
-	echo '[INFO] ------------------------------------------------------------------------'
-	echo '[INFO] Making books has FINISHED'
-	echo '[INFO] Now edit Installation_Guide/en-US/Author_Group.xml by hand'	
-	echo '[INFO] ------------------------------------------------------------------------'	
-
-}
-COMMENT
-
-function makepub {
-
-	#make publican version
-	echo '[INFO] Making publican version'
-	mkdir User_Guide
-	cp -r JBoss_Tools_User_Guide/en-US User_Guide/.
-	rm -rf User_Guide/en-US/Common_Content
-	rm -rf User_Guide/en-US/Book_info.xml
-	rm -rf User_Guide/en-US/Author_Group.xml
-	rm -rf User_Guide/en-US/images/ServerTools/JBT
-	cp pub/publican.cfg User_Guide/.
-	cp pub/Author_Group.xml User_Guide/en-US/.
-	cp pub/Book_Info.xml User_Guide/en-US/.	
-
-	#give feeback
-	echo '[INFO] ------------------------------------------------------------------------'
-	echo '[INFO] Making the publican book has FINISHED'
-	echo '[INFO] ------------------------------------------------------------------------'	
-
-}
-
-
-
-function build {
-
+	csprocessor build 22443 --flatten
+	unzip User_Guide.zip
+	rm User_Guide.zip	
+	sed -i 's/<firstname>.*<\/firstname>/<firstname>Red Hat<\/firstname>/' User_Guide/en-US/Author_Group.xml; sed -i 's/<surname>.*<\/surname>/<surname>Documentation Team<\/surname>/' User_Guide/en-US/Author_Group.xml
+	
 	#build publican version
 	echo '[INFO] Building publican version'
 	cd User_Guide
 	publican build
 	google-chrome tmp/en-US/html-single/index.html
-	cd ..
+	cd ..		
+
+	#give feeback
+	echo '[INFO] ------------------------------------------------------------------------'
+	echo '[INFO] Cleaning, making and building the publican version has FINISHED'
+	echo '[INFO] ------------------------------------------------------------------------'	
+		
+}
+
+
+function mvnmake {
+	#clean last version
+	echo '[INFO] Cleaning last version'	
+	rm -rf JBoss_Tools_User_Guide
+
+	#get updated content spec
+	echo '[INFO] Pulling latest content spec'
+	csprocessor pull 22477 -o JBoss_Tools_User_Guide.contentspec
+	rm JBoss_Tools_User_Guide.contentspec.backup
 	
-	#build mvn version
+	#make publican version
+	echo '[INFO] Making maven version'
+	csprocessor build 22477 --flatten --format jDocBook --hide-bug-links
+	unzip JBoss_Tools_User_Guide.zip
+	rm JBoss_Tools_User_Guide.zip	
+	sed -i 's/<firstname>.*<\/firstname>/<firstname>Red Hat<\/firstname>/' JBoss_Tools_User_Guide/en-US/Author_Group.xml; sed -i 's/<surname>.*<\/surname>/<surname>Documentation Team<\/surname>/' JBoss_Tools_User_Guide/en-US/Author_Group.xml
+	
+	#build publican version
 	echo '[INFO] Building maven version'
 	cd JBoss_Tools_User_Guide
 	mvn compile
 	google-chrome target/docbook/publish/en-US/html_single/index.html
-	cd ..
-	
-	#give feeback
-	echo '[INFO] ------------------------------------------------------------------------'
-	echo '[INFO] Building books has FINISHED'
-	echo '[INFO] ------------------------------------------------------------------------'	
-
-}	
-
-<<'COMMENT'
-function clean {
-	#clean publican version
-	echo '[INFO] Cleaning publican version'	
-	rm -rf Installation_Guide
-	
-	#clean maven version
-	echo '[INFO] Cleaning maven version'	
-	rm -rf Red_Hat_JBoss_Developer_Studio_Installation_Guide	
+	cd ..		
 
 	#give feeback
 	echo '[INFO] ------------------------------------------------------------------------'
-	echo '[INFO] Clearing books has FINISHED'
-	echo '[INFO] ------------------------------------------------------------------------'	
-		
-}	
-COMMENT
-
-function gitclean {
-	#clean publican version
-	echo '[INFO] Cleaning publican version'	
-	rm -rf User_Guide	
-
-	#give feeback
-	echo '[INFO] ------------------------------------------------------------------------'
-	echo '[INFO] Clearing books for GIT has FINISHED'
-	echo '[INFO] ------------------------------------------------------------------------'	
-		
-}
-
-
-function puball {
-	
-	gitclean
-	
-	makepub
-	
-	#build publican version
-	echo '[INFO] Building publican version'
-	cd User_Guide
-	publican build
-	google-chrome tmp/en-US/html-single/index.html
-	cd ..
-	
-	#give feeback
-	echo '[INFO] ------------------------------------------------------------------------'
-	echo '[INFO] Cleaning, making and building the publican book has FINISHED'
+	echo '[INFO] Cleaning, making and building the maven version has FINISHED'
 	echo '[INFO] ------------------------------------------------------------------------'	
 		
 }
@@ -143,21 +80,12 @@ function puball {
 #Main function
 #############################################################
 
-if [ $1 = "makepub" ]
+if [ $1 = "pubmake" ]
 then
-	makepub
-elif [ $1 = "build" ]
+	pubmake
+elif [ $1 = "mvnmake" ]
 then
-	build	
-elif [ $1 = "clean" ]
-then
-	clean
-elif [ $1 = "gitclean" ]
-then
-	gitclean
-elif [ $1 = "puball" ]
-then
-	puball	
+	mvnmake	
 else
-	echo '[INFO] Invalid command, choose from: makepub, build, gitclean, puball'
+	echo '[INFO] Invalid command, choose from: pubmake, mvnmake'
 fi
