e7ce88d1c6f3258f3aa7663da275628623934fef
Michelle Murray <mmurray@redhat.com>
2013-10-10 15:28:49 +1000

    TOOLSDOC-402: Release Notes - skeleton doc for 7.1.0

---
+++ docs/Release_Notes/Release_Notes_7.1.0/Release_Notes_7.1.0/en-US/files/pressgang_website.js
@@ -0,0 +1,9 @@
+pressgang_website_callback([
+	{"topicId":24019,"target":"Revision_History50","title":"Revision History","newSince":""},
+	{"topicId":24018,"target":"Resolved_Issues4","title":"Resolved Issues","newSince":""},
+	{"topicId":24017,"target":"Known_Issues11","title":"Known Issues","newSince":""},
+	{"topicId":24016,"target":"Apply_this_Release","title":"Apply this Release","newSince":""},
+	{"topicId":24020,"target":"New_Features6","title":"New Features","newSince":""},
+	{"topicId":22588,"target":"Use_Cases_of_JBoss_Developer_Studio1","title":"Use Cases of JBoss Developer Studio","newSince":""},
+	{"topicId":22589,"target":"About_Red_Hat_JBoss_Developer_Studio1","title":"About Red Hat JBoss Developer Studio","newSince":""},
+	{"topicId":24015,"target":"About_this_Release","title":"About this Release","newSince":""}]);
