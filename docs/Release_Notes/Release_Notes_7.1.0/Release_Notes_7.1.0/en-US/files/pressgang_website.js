76be615187b0bf00f57156e316afd5473ccf9fca
Michelle Murray <mmurray@redhat.com>
2013-11-01 17:20:39 +1000

    TOOLSDOC-402: Release Notes - addition of new features from B1N&N

--- docs/Release_Notes/Release_Notes_7.1.0/Release_Notes_7.1.0/en-US/files/pressgang_website.js
+++ docs/Release_Notes/Release_Notes_7.1.0/Release_Notes_7.1.0/en-US/files/pressgang_website.js
@@ -5,5 +5,6 @@ pressgang_website_callback([
 	{"topicId":24016,"target":"Apply_this_Release","title":"Apply this Release","newSince":""},
 	{"topicId":24020,"target":"New_Features6","title":"New Features","newSince":""},
 	{"topicId":22588,"target":"Use_Cases_of_JBoss_Developer_Studio1","title":"Use Cases of JBoss Developer Studio","newSince":""},
+	{"topicId":23618,"target":"Getting_Help_and_Giving_Feedback2","title":"Getting Help and Giving Feedback","newSince":""},
 	{"topicId":22589,"target":"About_Red_Hat_JBoss_Developer_Studio1","title":"About Red Hat JBoss Developer Studio","newSince":""},
 	{"topicId":24015,"target":"About_this_Release","title":"About this Release","newSince":""}]);
