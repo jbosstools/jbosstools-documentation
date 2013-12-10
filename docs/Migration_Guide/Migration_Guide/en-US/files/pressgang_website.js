796bbbf02b63670a88a4072b4ac62d2acf8e3eb0
Michelle Murray <mmurray@redhat.com>
2013-12-10 16:45:50 +1000

    TOOLSDOC-428: Install, Migrate, Getting Started Guides - small edits/screen cap changes for GA

--- docs/Migration_Guide/Migration_Guide/en-US/files/pressgang_website.js
+++ docs/Migration_Guide/Migration_Guide/en-US/files/pressgang_website.js
@@ -1,11 +1,11 @@
 pressgang_website_callback([
 	{"topicId":22593,"target":"Upgrade_Methods1","title":"Upgrade Methods","newSince":""},
 	{"topicId":22614,"target":"Revision_History39","title":"Revision History","newSince":""},
-	{"topicId":21439,"target":"Beta_Product","title":"Beta Product","newSince":""},
 	{"topicId":22615,"target":"Change_the_Version_of_Eclipse_Being_Used_with_JBoss_Developer_Studio_BYOE","title":"Change the Version of Eclipse Being Used with JBoss Developer Studio BYOE","newSince":""},
 	{"topicId":22616,"target":"Change_the_Path_of_the_JVM_used_to_Run_JBoss_Developer_Studio","title":"Change the Path of the JVM used to Run JBoss Developer Studio","newSince":""},
 	{"topicId":22588,"target":"Use_Cases_of_JBoss_Developer_Studio1","title":"Use Cases of JBoss Developer Studio","newSince":""},
 	{"topicId":23618,"target":"Getting_Help_and_Giving_Feedback2","title":"Getting Help and Giving Feedback","newSince":""},
 	{"topicId":22589,"target":"About_Red_Hat_JBoss_Developer_Studio1","title":"About Red Hat JBoss Developer Studio","newSince":""},
 	{"topicId":22604,"target":"Upgrade_from_JBoss_Developer_Studio_6.x_and_Earlier","title":"Upgrade from JBoss Developer Studio 6.x and Earlier","newSince":""},
+	{"topicId":24063,"target":"Use_CordovaSim","title":"Author Group","newSince":""},
 	{"topicId":22704,"target":"Upgrade_from_JBoss_Developer_Studio_7.0","title":"Upgrade from JBoss Developer Studio 7.0","newSince":""}]);
