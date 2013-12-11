eefb369e7c81c1a68aa7cb3f3be5f0691ac1df38
Michelle Murray <mmurray@redhat.com>
2013-12-11 16:12:45 +1000

    Rebasing: All core guides - rebasing topics in CCMS

--- docs/Release_Notes/Release_Notes_7.1.0/Release_Notes_7.1.0/en-US/files/pressgang_website.js
+++ docs/Release_Notes/Release_Notes_7.1.0/Release_Notes_7.1.0/en-US/files/pressgang_website.js
@@ -1,11 +1,11 @@
 pressgang_website_callback([
 	{"topicId":24019,"target":"Revision_History50","title":"Revision History","newSince":""},
+	{"topicId":13338,"target":"Use_Cases_of_JBoss_Developer_Studio","title":"Use Cases of JBoss Developer Studio","newSince":""},
 	{"topicId":24018,"target":"Resolved_Issues4","title":"Resolved Issues","newSince":""},
 	{"topicId":24017,"target":"Known_Issues11","title":"Known Issues","newSince":""},
+	{"topicId":13337,"target":"About_Red_Hat_JBoss_Developer_Studio","title":"About Red Hat JBoss Developer Studio","newSince":""},
 	{"topicId":24016,"target":"Apply_this_Release","title":"Apply this Release","newSince":""},
 	{"topicId":24020,"target":"New_Features6","title":"New Features","newSince":""},
-	{"topicId":22588,"target":"Use_Cases_of_JBoss_Developer_Studio1","title":"Use Cases of JBoss Developer Studio","newSince":""},
 	{"topicId":23618,"target":"Getting_Help_and_Giving_Feedback2","title":"Getting Help and Giving Feedback","newSince":""},
-	{"topicId":22589,"target":"About_Red_Hat_JBoss_Developer_Studio1","title":"About Red Hat JBoss Developer Studio","newSince":""},
 	{"topicId":24015,"target":"About_this_Release","title":"About this Release","newSince":""},
 	{"topicId":24063,"target":"Use_CordovaSim","title":"Author Group","newSince":""}]);
