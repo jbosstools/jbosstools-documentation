d6973a8e395207f4637a853f21e785e6692e12f5
Michelle Murray <mmurray@redhat.com>
2014-04-11 15:12:48 +1000

    TOOLSDOC-452: RNs for 8.0.0.Beta1

---
+++ docs/Release_Notes/Release_Notes_8.0.0/Release_Notes_8.0.0/en-US/files/pressgang_website.js
@@ -0,0 +1,12 @@
+pressgang_website_callback([
+	{"topicId":13338,"target":"Use_Cases_of_JBoss_Developer_Studio","title":"Use Cases of JBoss Developer Studio","newSince":""},
+	{"topicId":13337,"target":"About_Red_Hat_JBoss_Developer_Studio","title":"About Red Hat JBoss Developer Studio","newSince":""},
+	{"topicId":30253,"target":"New_Features17","title":"New Features","newSince":""},
+	{"topicId":30252,"target":"About_this_Release3","title":"About this Release","newSince":""},
+	{"topicId":21439,"target":"Beta_Product","title":"Beta Product","newSince":""},
+	{"topicId":30255,"target":"Known_Issues18","title":"Known Issues","newSince":""},
+	{"topicId":30254,"target":"Apply_this_Release3","title":"Apply this Release","newSince":""},
+	{"topicId":30256,"target":"Resolved_Issues7","title":"Resolved Issues","newSince":""},
+	{"topicId":23618,"target":"Getting_Help_and_Giving_Feedback2","title":"Getting Help and Giving Feedback","newSince":""},
+	{"topicId":30230,"target":"appe-Release_Notes_8.0.0-Revision_History","title":"Revision History","newSince":""},
+	{"topicId":24063,"target":"Author_Group","title":"Author Group","newSince":""}], 22666, "Red Hat JBoss Developer Studio", "Release Notes 8.0.0", "8.0 Beta1");
